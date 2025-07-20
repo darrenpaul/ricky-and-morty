3d636c03ee8577869af306f9894c8984f8c20fb0
Darren <drobertpaul@gmail.com>
2025-07-20 12:48:21 +0100

    Fixed missing imports

--- src/__tests__/composables/search-param-string.spec.ts
+++ src/__tests__/composables/search-param-string.spec.ts
@@ -1,7 +1,7 @@
-import type { RouteLocationNormalizedLoaded, Router, useRoute, useRouter } from 'vue-router'
+import type { RouteLocationNormalizedLoaded, Router } from 'vue-router'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-
-import { useSearchParamString } from '@/composables/search-param-string.ts'
+import { useRoute, useRouter } from 'vue-router'
+import { useSearchParamString } from '../../composables/search-param-string'
 
 vi.mock('vue-router', () => ({
   useRoute: vi.fn(),
